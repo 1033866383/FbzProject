852bc38da892e08b7e65ee474a644446263d3959
fanbozhou <fanbozhou@zuoyebang.com>
2021-05-25 22:40:53 +0800

    add classpath and change RequestUitl.java get method

--- src/main/java/util/RequestUitl.java
+++ src/main/java/util/RequestUitl.java
@@ -7,7 +7,6 @@ import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
-
 public class RequestUitl {
     enum TYPE{
         GET, POST, PUT, DELETE
@@ -54,42 +53,41 @@ public class RequestUitl {
         conn.setDoInput(true);// 允许输入
         conn.setDoOutput(true);// 允许输出
         conn.setUseCaches(false); // 不允许使用缓存
-        conn.setRequestMethod("POST");
+        conn.setRequestMethod(TYPE.POST.name());
         conn.setRequestProperty("connection", "keep-alive");
         conn.setRequestProperty("Charsert", "UTF-8");
         conn.setRequestProperty("Content-Type", MULTIPART_FROM_DATA + ";boundary=" + BOUNDARY);
 
-        // 首先组拼文本类型的参数
-        StringBuilder sb = new StringBuilder();
+        StringBuilder headers = new StringBuilder();
         for (Map.Entry<String, Object> entry : params.entrySet())
         {
-            sb.append(PREFIX);
-            sb.append(BOUNDARY);
-            sb.append(LINEND);
-            sb.append("Content-Disposition: form-data; name=\"" + entry.getKey() + "\"" + LINEND);
-            sb.append("Content-Type: text/plain; charset=" + CHARSET + LINEND);
-            sb.append("Content-Transfer-Encoding: 8bit" + LINEND);
-            sb.append(LINEND);
-            sb.append(entry.getValue());
-            sb.append(LINEND);
+            headers.append(PREFIX);
+            headers.append(BOUNDARY);
+            headers.append(LINEND);
+            headers.append("Content-Disposition: form-data; name=\"" + entry.getKey() + "\"" + LINEND);
+            headers.append("Content-Type: text/plain; charset=" + CHARSET + LINEND);
+            headers.append("Content-Transfer-Encoding: 8bit" + LINEND);
+            headers.append(LINEND);
+            headers.append(entry.getValue());
+            headers.append(LINEND);
         }
 
         DataOutputStream outStream = new DataOutputStream(conn.getOutputStream());
-        outStream.write(sb.toString().getBytes());
+        outStream.write(headers.toString().getBytes());
         InputStream in = null;
-        // 发送文件数据
+        // send file data
         if (files != null){
             for (Map.Entry<String, File> file : files.entrySet())
             {
-                StringBuilder sb1 = new StringBuilder();
-                sb1.append(PREFIX);
-                sb1.append(BOUNDARY);
-                sb1.append(LINEND);
+                StringBuilder data = new StringBuilder();
+                data.append(PREFIX);
+                data.append(BOUNDARY);
+                data.append(LINEND);
                 // name是post中传参的键 filename是文件的名称
-                sb1.append("Content-Disposition: form-data; name=\"file\"; filename=\"" + file.getKey() + "\"" + LINEND);
-                sb1.append("Content-Type: application/octet-stream; charset=" + CHARSET + LINEND);
-                sb1.append(LINEND);
-                outStream.write(sb1.toString().getBytes());
+                data.append("Content-Disposition: form-data; name=\"file\"; filename=\"" + file.getKey() + "\"" + LINEND);
+                data.append("Content-Type: application/octet-stream; charset=" + CHARSET + LINEND);
+                data.append(LINEND);
+                outStream.write(data.toString().getBytes());
 
                 InputStream is = new FileInputStream(file.getValue());
                 byte[] buffer = new byte[1024];
@@ -102,11 +100,9 @@ public class RequestUitl {
                 outStream.write(LINEND.getBytes());
             }
 
-            // 请求结束标志
             byte[] end_data = (PREFIX + BOUNDARY + PREFIX + LINEND).getBytes();
             outStream.write(end_data);
             outStream.flush();
-            // 得到响应码
             int res = conn.getResponseCode();
             if (res == 200){
                 in = conn.getInputStream();
@@ -198,7 +194,7 @@ public class RequestUitl {
     private static Response sendData(String url, Map<String, String> headers, Map<String, Object> params, String type, boolean isJson) throws IOException {
         HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(url).openConnection();
         httpURLConnection.setRequestMethod(type);
-        if (isJson == true) {
+        if (isJson) {
             httpURLConnection.setRequestProperty("Content-Type", "application/json");
         } else {
             httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
@@ -252,7 +248,7 @@ public class RequestUitl {
             byteChange.put(tmp);
         Map<String, List<String>> setHeaders = httpURLConnection.getHeaderFields();
         inputStream.close();
-        return new Response(httpURLConnection.getResponseCode(), new String(byteChange.getByte(), "UTF-8"), setHeaders);
+        return new Response(httpURLConnection.getResponseCode(), new String(byteChange.getByte()), setHeaders);
     }
 
 
