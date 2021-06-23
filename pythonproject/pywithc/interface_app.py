055008039aacfe7478653893c5517ce6a2f43bd9
1033866383 <56209295+1033866383@users.noreply.github.com>
2021-06-23 22:32:28 +0800

    Update interface_app.py

--- pythonproject/pywithc/interface_app.py
+++ pythonproject/pywithc/interface_app.py
@@ -8,15 +8,14 @@ from literacy_test_project.interface.auto_test_app import read_data
 
 @read_data
 class auto:
-    zybuss = None
-
+   
     #Here, the results of all methods can be obtained if one method depends on another method.
     res_func = dict()
 
     #run order function name, if not have property will use default hash order
     run_order = ["send_code", "login_shizi", "user_info"]
 
-    hosts = {"dev": "http://shizi-api-dev-e.suanshubang.cc", "test": "http://shizi-api-test-e.suanshubang.cc"}
+    hosts = {"dev": "dev", "test": "test"}
 
     URL = hosts.get(config.env)
 
@@ -26,25 +25,23 @@ class auto:
 
 
     def send_code(self, data):
-        url = "/shizi-api/auth/sendCode"
+        url = "sendCode"
         return requests.post(url= self.URL + url, data=data).text
 
 
     def login_shizi(self, data):
-        url = "/shizi-api/auth/login"
+        url = "login"
         res = requests.post(url = self.URL + url, data=data).text
-        self.zybuss = json.loads(res).get("data").get("zybuss")
         return res
 
     def version(self, data):
-        url = "/shizi-api/version/data"
+        url = "data"
         return requests.get(url= self.URL + url, params=data).text
 
 
     def user_info(self, data):
         # 用户信息
-        url = "/api/user/info"
-        data["ZYBUSS"] = self.zybuss
+        url = "info"
         return requests.get(url=self.URL + url, params=data).text
 
 
